a74f97fcedc9c4276190e4860a8c53cc7207f33e
Lukas Turcani <lukasturcani93@gmail.com>
2023-09-15 17:15:29 +0100

    Add test data

--- tests/test_nmr.py
+++ tests/test_nmr.py
@@ -1,4 +1,3 @@
-from collections.abc import Iterator
 from pathlib import Path
 
 import cagey
@@ -9,12 +8,17 @@ from sqlmodel import Session, SQLModel, create_engine
 from sqlmodel.pool import StaticPool
 
 
-def test_nmr_extraction(engine: Engine) -> None:
+def test_nmr_extraction(engine: Engine, datadir: Path) -> None:
     with Session(engine) as session:
-        cagey.nmr.add_data(Path("/home/lukas/downloads/NMR_data"), session)
-        df = pl.read_database("SELECT * FROM aldehydepeak", engine.connect())
-        print(df)
-        assert False
+        cagey.nmr.add_data(datadir / "NMR_data", session)
+        aldehyde_peaks = pl.read_database(
+            "SELECT * FROM aldehydepeak", engine.connect()
+        )
+        assert len(aldehyde_peaks) == 3
+        imine_peaks = pl.read_database(
+            "SELECT * FROM iminepeak", engine.connect()
+        )
+        assert len(imine_peaks) == 35
 
 
 @fixture
