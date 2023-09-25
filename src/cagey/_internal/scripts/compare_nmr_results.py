1ebf120ca6e06448f76eff304ac7f67bf86996ba
Lukas Turcani <lukasturcani93@gmail.com>
2023-09-25 17:34:20 +0100

    print stuff

--- src/cagey/_internal/scripts/compare_nmr_results.py
+++ src/cagey/_internal/scripts/compare_nmr_results.py
@@ -16,24 +16,35 @@ def main() -> None:
         poolclass=StaticPool,
     )
     old_results = pl.read_csv(args.csv_results)
+    missing = 0
+    not_missing = 0
     with Session(engine) as session:
         for row in old_results.rows(named=True):
+            if "PLATE2B" in row["Plate_Name"]:
+                plate = "P4"
+            else:
+                plate = row["Plate_Name"].replace("PLATE", "P")
             statement = select(NmrSpectrum).where(
                 NmrSpectrum.experiment == row["Experiment_Name"],
-                NmrSpectrum.plate == row["Plate_Name"].replace("PLATE", "P"),
+                NmrSpectrum.plate == plate,
                 NmrSpectrum.formulation_number == row["Formulation_Number"],
             )
             nmr_spectrum = session.exec(statement).one_or_none()
             if nmr_spectrum is None:
+                print(
+                    row["Experiment_Name"],
+                    row["Plate_Name"],
+                    row["Formulation_Number"],
+                )
+                missing += 1
                 continue
-            print(
-                f"{nmr_spectrum.experiment} {nmr_spectrum.plate} "
-                f"{nmr_spectrum.formulation_number}"
-            )
+            not_missing += 1
             has_imine = len(nmr_spectrum.imine_peaks) > 0
             assert row["Comp_Imine_Check"] == has_imine
             has_aldehyde = len(nmr_spectrum.aldehyde_peaks) > 0
             assert row["Comp_Aldehyde_Check"] == has_aldehyde
+    print(f"Missing: {missing}")
+    print(f"Not missing: {not_missing}")
 
 
 def _parse_args() -> argparse.Namespace:
