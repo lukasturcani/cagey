ad10e8498595f3e355df541a0abf3a822f97d3df
Lukas Turcani <lukasturcani93@gmail.com>
2023-09-27 17:52:26 +0100

    improve prints

--- src/cagey/_internal/scripts/print_tables.py
+++ src/cagey/_internal/scripts/print_tables.py
@@ -15,50 +15,60 @@ def main() -> None:
     )
     if args.verbose:
         pl.Config.set_tbl_rows(-1)
+        pl.Config.set_tbl_cols(-1)
     if "nmr" in args.tables:
         nmr_spectra = pl.read_database(
             "SELECT * FROM nmrspectrum", engine.connect()
         )
+        print("nmrspectrum")
         print(nmr_spectra)
     if "aldehyde" in args.tables:
         aldehyde_peaks = pl.read_database(
             "SELECT * FROM aldehydepeak", engine.connect()
         )
+        print("aldehydepeak")
         print(aldehyde_peaks)
     if "imine" in args.tables:
         imine_peaks = pl.read_database(
             "SELECT * FROM iminepeak", engine.connect()
         )
+        print("iminepeak")
         print(imine_peaks)
     if "precursor" in args.tables:
         precursors = pl.read_database(
             "SELECT * FROM precursor", engine.connect()
         )
+        print("precursor")
         print(precursors)
     if "reaction" in args.tables:
         reactions = pl.read_database(
             "SELECT * FROM reaction", engine.connect()
         )
+        print("reaction")
         print(reactions)
     if "mass_spec" in args.tables:
         mass_spec = pl.read_database(
             "SELECT * FROM massspectrum", engine.connect()
         )
+        print("massspectrum")
         print(mass_spec)
     if "corrected_mass_spec_peak" in args.tables:
         corrected_mass_spec_peaks = pl.read_database(
             "SELECT * FROM correctedmassspecpeak", engine.connect()
         )
+        print("correctedmassspecpeak")
         print(corrected_mass_spec_peaks)
     if "mass_spec_peak" in args.tables:
         mass_spec_peaks = pl.read_database(
             "SELECT * FROM massspecpeak", engine.connect()
         )
+        print("massspecpeak")
         print(mass_spec_peaks)
     if "mass_spec_topology" in args.tables:
         mass_spec_topology_assignments = pl.read_database(
             "SELECT * FROM massspectopologyassignment", engine.connect()
         )
+        print("massspectopologyassignment")
         print(mass_spec_topology_assignments)
 
 
