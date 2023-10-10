293ff67b78ce9fc7c11c3ae5abd0abe8268c5b16
Lukas Turcani <lukasturcani93@gmail.com>
2023-10-10 15:15:00 +0100

    refactor

--- src/cagey/_internal/scripts/compare_nmr_results.py
+++ src/cagey/_internal/scripts/compare_nmr_results.py
@@ -10,6 +10,7 @@ from sqlmodel.pool import StaticPool
 
 
 def main() -> None:
+    pl.Config.set_tbl_cols(-1)
     args = _parse_args()
     engine = create_engine(
         f"sqlite:///{args.database}",
@@ -18,8 +19,8 @@ def main() -> None:
     )
     old = _get_old_nmr_results(map(OldData.from_path, args.csv_results))
     new = _get_new_nmr_results(engine)
-    combined = old.join(
-        new,
+    combined_summary = old.summary.join(
+        new.summary,
         on=["experiment", "plate", "formulation_number"],
         how="outer",
         suffix="_new",
@@ -29,8 +30,8 @@ def main() -> None:
             & (pl.col("has_imine") == pl.col("has_imine_new"))
         ),
     )
-    summary = combined.group_by("match").agg(pl.count()).collect()
-    print(summary)
+    print(combined_summary.group_by("match").agg(pl.count()).collect())
+    print(old.aldehyde_peaks.collect())
 
 
 def _parse_args() -> argparse.Namespace:
@@ -50,8 +51,15 @@ class OldData:
         return OldData(path.stem, path)
 
 
-def _get_old_nmr_results(paths: Iterable[OldData]) -> pl.LazyFrame:
-    return pl.concat(
+@dataclass(frozen=True, slots=True)
+class Results:
+    summary: pl.LazyFrame
+    aldehyde_peaks: pl.LazyFrame
+    imine_peaks: pl.LazyFrame
+
+
+def _get_old_nmr_results(paths: Iterable[OldData]) -> Results:
+    results = pl.concat(
         pl.scan_csv(path.path)
         .rename(
             {
@@ -59,34 +67,108 @@ def _get_old_nmr_results(paths: Iterable[OldData]) -> pl.LazyFrame:
                 "Position": "formulation_number",
                 "Aldehyde?": "has_aldehyde",
                 "Imine?": "has_imine",
+                "Aldehyde_Peaks": "aldehyde_peaks",
+                "Imine_Peaks": "imine_peaks",
+                "Aldehyde_Amplitudes": "aldehyde_amplitudes",
+                "Imine_Amplitudes": "imine_amplitudes",
             }
         )
         .with_columns(experiment=pl.lit(path.experiment))
         for path in paths
     )
+    return Results(
+        summary=results.select(
+            [
+                "experiment",
+                "plate",
+                "formulation_number",
+                "has_aldehyde",
+                "has_imine",
+            ]
+        ),
+        aldehyde_peaks=_explode_peak_columns(
+            results.select(
+                [
+                    "experiment",
+                    "plate",
+                    "formulation_number",
+                    "aldehyde_peaks",
+                    "aldehyde_amplitudes",
+                ]
+            ),
+            "aldehyde_peaks",
+            "aldehyde_amplitudes",
+        ).rename(
+            {"aldehyde_peaks": "ppm", "aldehyde_amplitudes": "amplitude"}
+        ),
+        imine_peaks=_explode_peak_columns(
+            results.select(
+                [
+                    "experiment",
+                    "plate",
+                    "formulation_number",
+                    "imine_peaks",
+                    "imine_amplitudes",
+                ]
+            ),
+            "imine_peaks",
+            "imine_amplitudes",
+        ).rename({"imine_peaks": "ppm", "imine_amplitudes": "amplitude"}),
+    )
 
 
-def _get_new_nmr_results(engine: Engine) -> pl.LazyFrame:
-    has_aldehyde_peaks = (
-        pl.read_database(
-            "SELECT nmr_spectrum_id FROM nmraldehydepeak", engine.connect()
+def _explode_peak_columns(
+    df: pl.LazyFrame, ppm: str, amplitude: str
+) -> pl.LazyFrame:
+    return (
+        df.with_columns(
+            pl.col(ppm).str.strip_chars("[]"),
+            pl.col(amplitude).str.strip_chars("[]"),
         )
-        .lazy()
-        .unique("nmr_spectrum_id")
-        .with_columns(has_aldehyde=True)
-    )
-    has_imine_peaks = (
-        pl.read_database(
-            "SELECT nmr_spectrum_id FROM nmriminepeak", engine.connect()
+        .filter(
+            pl.col(ppm).is_not_null()
+            & pl.col(amplitude).is_not_null()
+            & (pl.col(ppm) != "")
+            & (pl.col(amplitude) != "")
         )
-        .lazy()
-        .unique("nmr_spectrum_id")
-        .with_columns(has_imine=True)
+        .with_columns(
+            pl.col(ppm)
+            .str.split(pl.lit(","))
+            .list.eval(
+                pl.element().str.strip_chars().cast(pl.Float64), parallel=True
+            ),
+            pl.col(amplitude)
+            .str.split(pl.lit(","))
+            .list.eval(
+                pl.element().str.strip_chars().cast(pl.Float64), parallel=True
+            ),
+        )
+        .explode([ppm, amplitude])
     )
+
+
+def _get_new_nmr_results(engine: Engine) -> Results:
+    aldehyde_peaks = pl.read_database(
+        "SELECT nmr_spectrum_id, ppm, amplitude  FROM nmraldehydepeak",
+        engine.connect(),
+    ).lazy()
+    has_aldehyde_peaks = aldehyde_peaks.unique("nmr_spectrum_id").with_columns(
+        has_aldehyde=True
+    )
+
+    imine_peaks = pl.read_database(
+        "SELECT nmr_spectrum_id, ppm, amplitude FROM nmriminepeak",
+        engine.connect(),
+    ).lazy()
+    has_imine_peaks = imine_peaks.unique("nmr_spectrum_id").with_columns(
+        has_imine=True
+    )
+
     has_peaks = has_aldehyde_peaks.join(
         has_imine_peaks, on="nmr_spectrum_id", how="outer"
     ).fill_null(False)
-    return (
+
+    nmr_spectra = (
         pl.read_database(
             "SELECT id, experiment, plate, formulation_number "
             "FROM nmrspectrum ",
@@ -94,8 +176,15 @@ def _get_new_nmr_results(engine: Engine) -> pl.LazyFrame:
         )
         .lazy()
         .rename({"id": "nmr_spectrum_id"})
-        .join(has_peaks, on="nmr_spectrum_id", how="left")
-        .drop("nmr_spectrum_id")
+    )
+    return Results(
+        summary=nmr_spectra.join(has_peaks, on="nmr_spectrum_id", how="left"),
+        aldehyde_peaks=aldehyde_peaks.join(
+            nmr_spectra, on="nmr_spectrum_id", how="left"
+        ),
+        imine_peaks=imine_peaks.join(
+            nmr_spectra, on="nmr_spectrum_id", how="left"
+        ),
     )
 
 
