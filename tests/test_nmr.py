79d52c2915ddd42c21939f94a67e2bc53fb16972
Lukas Turcani <lukasturcani93@gmail.com>
2023-10-23 16:02:53 +0100

    stuff

--- tests/test_nmr.py
+++ tests/test_nmr.py
@@ -7,27 +7,14 @@ from cagey import Reaction
 
 def test_nmr_extraction(datadir: Path) -> None:
     spectrum = cagey.nmr.get_spectrum(
-        datadir / "AB-02-005" / "P1" / "230" / "pdata" / "1",
+        datadir / "NMR_data" / "AB-02-005" / "P1" / "170" / "pdata" / "1",
         Reaction(id=1, experiment="AB-02-005", plate=1, formulation_number=23),
     )
     aldehyde_peaks = sorted(
         map(attrgetter("ppm", "amplitude"), spectrum.aldehyde_peaks)
     )
-    assert aldehyde_peaks == [
-        (
-            9.085202304063953,
-            31739.5703125,
-        ),
-        (
-            9.087647317197327,
-            71858.1640625,
-        ),
-        (
-            9.091926090180733,
-            10483.015625,
-        ),
-    ]
+    assert aldehyde_peaks == []
     imine_peaks = sorted(
-        map(attrgetter("ppm", "amplitude"), spectrum.aldehyde_peaks)
+        map(attrgetter("ppm", "amplitude"), spectrum.imine_peaks)
     )
     assert imine_peaks == []
