938f3c4ceaa34f822da1f5e823edb22714fb1b73
Lukas Turcani <lukasturcani93@gmail.com>
2023-11-01 11:46:03 +0000

    Add script

---
+++ src/cagey/_internal/scripts/rename_ms_folder.py
@@ -0,0 +1,33 @@
+import argparse
+from pathlib import Path
+from typing import cast
+
+
+def main() -> None:
+    args = _parse_args()
+    for folder in cast(list[Path], args.folder):
+        try:
+            experiment, plate_data = folder.stem.split("_")
+            plate, formulation_number = plate_data.split("-")
+        except ValueError:
+            print(f"bad folder: {folder}")
+            continue
+
+        stem = "_".join(
+            [
+                experiment,
+                plate.removeprefix("P").replace("2B", "4").zfill(2),
+                formulation_number.zfill(2),
+            ]
+        )
+        folder.rename(folder.parent / f"{stem}.d")
+
+
+def _parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("folder", type=Path, nargs="+")
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
