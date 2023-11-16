c12723bdf78bd152a9aa68c73c823eddb573b50b
Lukas Turcani <lukasturcani93@gmail.com>
2023-11-16 15:09:33 +0000

    add script

---
+++ src/cagey/_internal/scripts/add_turbidity.py
@@ -0,0 +1,32 @@
+import argparse
+import json
+from pathlib import Path
+from typing import TypeAlias
+from enum import Enum, auto
+
+Json: TypeAlias = int | float | str | None | list["Json"] | dict[str, "Json"]
+
+
+class TurbidState(Enum):
+    DISSOLVED = auto()
+    SATURATED = auto()
+    STABLE = auto()
+    UNSTABLE = auto()
+
+
+def main() -> None:
+    args = _parse_args()
+    for path in args.data:
+        data = _read_json(path)
+
+
+def _parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("database", type=Path)
+    parser.add_argument("data", type=Path, nargs="+")
+    return parser.parse_args()
+
+
+def _read_json(path: Path) -> Json:
+    with path.open() as file:
+        return json.load(file)
