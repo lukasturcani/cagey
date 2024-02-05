eacbc94409e9b53140ed7a7712c01a8352e012e7
Lukas Turcani <lukasturcani93@gmail.com>
2024-02-05 10:48:48 +0000

    Add docs

--- src/cagey/_internal/scripts/cagey.py
+++ src/cagey/_internal/scripts/cagey.py
@@ -3,10 +3,12 @@ from typing import Annotated
 
 import typer
 from rich import print
-from rich.console import Group
+from rich.console import Console, Group
 from rich.markup import escape
 from rich.panel import Panel
 from rich.tree import Tree
+
+console = Console()
 
 app = typer.Typer(
     no_args_is_help=True,
@@ -18,32 +20,59 @@ app = typer.Typer(
 def main() -> None:
     """A cage database tool.
 
-    [bright_magenta]cagey[/] creates and manages a database of cage data so
-    that you don't have to and can focus on data analysis.
-
-    [bold green underline]What it's for[/]
-
-    We collect a range of experimental and computational data about cages,
-    including their precursors, mass spectra, NMR and so on. This data
-    comes in a range of dissimilar formats and is located across a broad range
-    of files. Not so good for doing data analysis! [bright_magenta]cagey[/]
-    helps you to organise this data into a single database, so that you can
-    easily share your data with other people, as all your data will be stored
-    in a single file, and so that you can easily analyse your data, as it will
-    be stored in a standardised and well documented format.
-
-    [bold green underline]How it works[/]
-
-    First your create a new database using the \
-[bright_magenta]cagey[/] [bold green]new[/] command.
-
-    [bold green underline]How to use it[/]
-
-    [birght_magenta]cagey[/] is a command line tool composed of several
-    sub-commands.
-
+    Run [bright_magenta]cagey[/] [blue]help[/] for an introduction.
     """
     return
+
+
+@app.command()
+def help() -> None:
+    """Get help on how to use [bright_magenta]cagey[/]."""
+    console.print(
+        """
+[bright_magenta]cagey[/] creates and manages a database of cage data so \
+that you don't have to and can focus on data analysis.
+
+[bold green underline]What it's for[/]
+
+We collect a range of experimental and computational data about cages, \
+including their precursors, mass spectra, NMR and so on. This data \
+comes in a range of dissimilar formats and is located across a broad range \
+of files. Not so good for doing data analysis! [bright_magenta]cagey[/] \
+helps you to organise this data into a single database, so that you can \
+easily share your data with other people, as all your data will be stored \
+in a single file, and so that you can easily analyse your data, as it will \
+be stored in a standardised and well documented format.
+
+[bold green underline]How it works[/]
+
+First your create a new database using the \
+[bright_magenta]cagey[/] [green]new[/] command. This will create a new \
+database file and populate it with the data in the folder you provide. \
+The data in the folder should be organised in a standardised format, \
+described in help of the [green]new[/] command, i.e. when you run \
+[bright_magenta]cagey[/] [green]new[/] [blue]help[/]. Your database file \
+will contain all the data in the folder, but in a standardised format, \
+so that you can easily share it with other people and easily analyse it. \
+The data in the database is easily extracted into data frames, so that \
+you can work with polars, pandas, or any other data analysis library you \
+like. I strongly reccomend using polars - \
+https://docs.pola.rs/user-guide/getting-started - \
+as it is much faster than pandas, and has more intuitive API.
+
+The database you create is a standard SQLite database, meaing the data \
+comes in the form of tables, and you can use SQL to query the data. \
+You do not have to know SQL to use [bright_magenta]cagey[/], but it is \
+useful to know, as it will allow you to do more complex queries. If you \
+would like to learn SQL, I reccomend the following tutorial - \
+https://www.codecademy.com/learn/learn-sql - it takes about 5 hours to \
+complete.
+
+[bold green underline]How to use it[/]
+
+[birght_magenta]cagey[/] is a command line tool composed of several \
+sub-commands."""
+    )
 
 
 def print_folder_structure() -> None:
