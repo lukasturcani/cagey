ef56b440ba156899738f3db5f520e3ef88938cc0
Lukas Turcani <lukasturcani93@gmail.com>
2024-02-15 15:32:39 +0000

    Add stuff

--- src/cagey/_internal/scripts/cagey.py
+++ src/cagey/_internal/scripts/cagey.py
@@ -26,7 +26,7 @@ def main() -> None:
 
 
 @app.command()
-def help() -> None:
+def help() -> None:  # noqa: A001
     """Get help on how to use [bright_magenta]cagey[/]."""
     console.print(
         """
@@ -39,10 +39,10 @@ We collect a range of experimental and computational data about cages, \
 including their precursors, mass spectra, NMR and so on. This data \
 comes in a range of dissimilar formats and is located across a broad range \
 of files. Not so good for doing data analysis! [bright_magenta]cagey[/] \
-helps you to organise this data into a single database, so that you can \
-easily share your data with other people, as all your data will be stored \
-in a single file, and so that you can easily analyse your data, as it will \
-be stored in a standardised and well documented format.
+organises this data into a single database, allowing you to jump right \
+into the analysis of your data, as it will be stored in a standardised \
+and well documented format. You will also be able to share your data \
+with other people, as all your data will be stored in a single file.
 
 [bold green underline]How it works[/]
 
@@ -52,13 +52,13 @@ database file and populate it with the data in the folder you provide. \
 The data in the folder should be organised in a standardised format, \
 described in help of the [green]new[/] command, i.e. when you run \
 [bright_magenta]cagey[/] [green]new[/] [blue]help[/]. Your database file \
-will contain all the data in the folder, but in a standardised format, \
-so that you can easily share it with other people and easily analyse it. \
-The data in the database is easily extracted into data frames, so that \
+will contain all the data in the folder, but in a standardised format. \
+The data in the database can be extracted into data frames, so that \
 you can work with polars, pandas, or any other data analysis library you \
 like. I strongly reccomend using polars - \
 https://docs.pola.rs/user-guide/getting-started - \
-as it is much faster than pandas, and has more intuitive API.
+as it is much faster than pandas, and has more intuitive API. Worth it for \
+even the seasoned pandas user!
 
 The database you create is a standard SQLite database, meaning the data \
 comes in the form of tables, and you can use SQL to query the data. \
@@ -82,11 +82,19 @@ the [bright_magenta]cagey[/] database.
 
 [bright_magenta]cagey[/] is a command line tool composed of several \
 sub-commands. You can see a list of these sub-commands by running \
-[bright_magenta]cagey[/] [blue]help[/]. You can get help on how to use \
+[bright_magenta]cagey[/]. You can get help on how to use \
 each sub-command by running \
 [bright_magenta]cagey[/] [green]sub-command[/] [blue]help[/]. For example, \
 to get help on how to use the [green]new[/] sub-command, you would run \
-[bright_magenta]cagey[/] [green]new[/] [blue]help[/]. """
+[bright_magenta]cagey[/] [green]new[/] [blue]help[/].
+
+Normally you will use [bright_magenta]cagey[/] [green]new[/] to create a new \
+database, and [bright_magenta]cagey[/] [green]insert[/] to insert new data \
+into the database. That's pretty much it! Once you have your database you can \
+write your own Python scripts to read the data held by it and perform data \
+analysis on it. Look at https://cagey.readthedocs.io for examples of how to \
+do this.
+"""
     )
 
 
