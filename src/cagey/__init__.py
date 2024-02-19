c56fc98beee2906a9861c630d3faae55628ea1c9
Lukas Turcani <lukasturcani93@gmail.com>
2024-02-19 12:22:39 +0000

    remove stuff

--- src/cagey/__init__.py
+++ src/cagey/__init__.py
@@ -1,11 +1,10 @@
 """Streamlined automated data analysis."""
 
-from cagey import ms, nmr, notebook, queries, reactions, turbidity
+from cagey import ms, nmr, queries, reactions, turbidity
 
 __all__ = [
     "ms",
     "nmr",
-    "notebook",
     "reactions",
     "queries",
     "turbidity",
