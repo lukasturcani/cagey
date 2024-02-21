1c20fb4d6d2501c8772ba53f626df9fbbc081263
Lukas Turcani <lukasturcani93@gmail.com>
2024-02-21 14:53:52 +0000

    wip

--- src/cagey/turbidity.py
+++ src/cagey/turbidity.py
@@ -1,9 +1,7 @@
 """Turbidity analysis."""
 
-from cagey._internal.queries import TurbidState
 from cagey._internal.turbidity import get_turbid_state
 
 __all__ = [
-    "TurbidState",
     "get_turbid_state",
 ]
