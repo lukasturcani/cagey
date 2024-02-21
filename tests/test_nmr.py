c50e40528dc8f30efa59b5250aa5cfbd54cefe6c
Lukas Turcani <lukasturcani93@gmail.com>
2024-02-21 21:13:31 +0000

    wip

--- tests/test_nmr.py
+++ tests/test_nmr.py
@@ -2,13 +2,11 @@ from operator import attrgetter
 from pathlib import Path
 
 import cagey
-from cagey import Reaction
 
 
 def test_nmr_extraction(datadir: Path) -> None:
     spectrum = cagey.nmr.get_spectrum(
         datadir / "NMR_data" / "AB-02-005" / "P1" / "170" / "pdata" / "1",
-        Reaction(id=1, experiment="AB-02-005", plate=1, formulation_number=23),
     )
     aldehyde_peaks = sorted(
         map(attrgetter("ppm", "amplitude"), spectrum.aldehyde_peaks)
