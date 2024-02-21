5a7f9b2ed506d291f12dae99b673d2d15c752321
Lukas Turcani <lukasturcani93@gmail.com>
2024-02-21 21:58:49 +0000

    Fix tests

--- tests/test_nmr.py
+++ tests/test_nmr.py
@@ -15,4 +15,14 @@ def test_nmr_extraction(datadir: Path) -> None:
     imine_peaks = sorted(
         map(attrgetter("ppm", "amplitude"), spectrum.imine_peaks)
     )
-    assert imine_peaks == []
+    assert imine_peaks == sorted(
+        [
+            (8.24472903946652, 21402.3671875),
+            (8.184214964415505, 12020027.3828125),
+            (8.157931073231731, 10329.4296875),
+            (7.65181335462324, 10373.234375),
+            (7.6499795947732085, 10029.46875),
+            (7.589465519722193, 19573283.65625),
+            (7.384695669802092, 384487.59375),
+        ]
+    )
